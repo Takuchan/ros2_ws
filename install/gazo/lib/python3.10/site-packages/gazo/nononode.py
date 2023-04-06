7e2e0e81e9c0b1d330f7c3578716cbfd52fa1fb4
Takuchan <takuchanapp@gmail.com>
2023-04-07 01:33:49 +0900

    :ADD: Add a program that calls the callback many times

--- install/gazo/lib/python3.10/site-packages/gazo/nononode.py
+++ install/gazo/lib/python3.10/site-packages/gazo/nononode.py
@@ -5,12 +5,18 @@ class HappyNode(Node):
   def __init__(self):
     print("I'm love it!")
     super().__init__('happy_node')
-    self.get_logger().info('HappyWorld!!')
+    for a in range(100):
+      self.get_logger().info('HappyWorld!!')
+    self.timer = self.create_timer(1.0,self.timer_callback)
+
+  def timer_callback(self):
+    self.get_logger().info('HappyWororororo!!')
 
 def main():
     print('Hi from gazo.')
     rclpy.init()
     node = HappyNode()
+    rclpy.spin(node)
     rclpy.shutdown()
     print('program end')
 
